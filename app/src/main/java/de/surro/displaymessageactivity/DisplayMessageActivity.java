bc2346997b1f8bec8c5e99aa425a156badab2f49
Surro402 <alex.lautenschlaeger@googlemail.com>
2015-10-02 16:07:22 +0200

    Update DisplayMessageActivity.java

--- app/src/main/java/de/surro/displaymessageactivity/DisplayMessageActivity.java
+++ app/src/main/java/de/surro/displaymessageactivity/DisplayMessageActivity.java
@@ -6,9 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-/**
- * Created by Alexander Lautenschl on 02.10.2015.
- */
 public class DisplayMessageActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
